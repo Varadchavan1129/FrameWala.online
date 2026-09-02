9f9cfbe9bdac9481f044077fb173fb722bb532d7
Vedanti190307 <vedantipawar2007@gmail.com>
2026-09-02 16:10:15 +0530

    docs: clarify storefront mount point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import CustomerApp from './CustomerApp.jsx'
 import './assets/styles/index.css'
 
+// Mount the customer storefront at the root page element.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CustomerApp />
